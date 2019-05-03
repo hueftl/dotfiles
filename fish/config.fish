48c9763dd4da7c29545d5f97e4699ecfce442d13
Alexander Menne <amenne@ypsilon.net>
2019-05-03 09:21:23 +0200

    [Updated] Binary paths in fish config.

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,33 @@
-source $HOME/.config/fish/aliases.fish
+set -x VISUAL vim¬
+set -x EDITOR vim¬
 
 set -g theme_color_scheme gruvbox
 
-set -x VISUAL vim
-set -x EDITOR vim
+if test -d $HOME/.yarn/bin -a $HOME/.yarn-config
+    set -x YARNBIN $HOME/.yarn/bin
+    set -x YARNGLOBALBIN $HOME/.yarn-config/global/node_modules/.bin
+end
+
+if test -d $HOME/.cargo/bin
+    set -x CARGOBIN $HOME/.cargo/bin
+end
+
+if test -d /usr/bin/ccache
+    set -x CCACHEBIN /usr/bin/ccache
+end
+
+if test -d /usr/local/sbin
+    set -x LOCALSBIN /usr/local/sbin
+end
+
+if test -d /usr/local/bin
+    set -x USRLOCALBIN /usr/local/bin
+end
+
+if test -d /usr/sbin
+    set -x SBIN /usr/sbin
+end
+
+set -U fish_user_paths $YARNBIN $YARNGLOBALBIN $CARGOBIN $LOCALSBIN $USRLOCALBIN $SBIN
+
+source $HOME/.config/fish/aliases.fish
